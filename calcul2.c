7d33a8741cca324e86f7981813a92dfe6829362e
k3r0z3ne <126825599+k3r0z3ne@users.noreply.github.com>
2024-01-22 14:47:07 +0100

    Add files via upload

---
+++ calcul2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int resultat = 0, nombre1 = 0, nombre2 = 0, nombre3 = 0;
+    printf("Entrez la valeur [1]\n");
+    scanf("%d", &nombre1);
+
+    printf("Entrez la valeur [2]\n");
+    scanf("%d", &nombre2);
+
+    printf("Entrez la valeur [3]\n");
+    scanf("%d", &nombre3);
+
+    resultat = nombre1 * nombre2 / nombre3;
+
+    printf("%d * %d / %d = %d\n", nombre1, nombre2, nombre3, resultat);
+
+    return 0;
+}
